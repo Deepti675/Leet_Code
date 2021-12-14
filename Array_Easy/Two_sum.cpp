775403cd96f2a369bff5ee4bc1be84b4ef073074
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2021-12-14 14:58:41 +0530

    Update Two_sum.cpp

--- Array_Easy/Two_sum.cpp
+++ Array_Easy/Two_sum.cpp
@@ -1,1 +1,25 @@
-
+/*Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].*/
+//brute force method solution//
+ class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>v;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[j]==target-nums[i])
+                {
+                    v={i,j};
+                    return v;
+                }
+            }
+        }
+        return v;
+        }
+        
+    
+};
+//Hash Map method//
