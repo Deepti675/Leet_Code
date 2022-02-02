65eb5a3dc55a43c00b2463a1f1b3948bd6d76bb9
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-02-02 12:54:10 +0530

    Create Array_Permutation.cpp

---
+++ Array_Easy/Array_Permutation.cpp
@@ -0,0 +1,30 @@
+
+//link- https://leetcode.com/problems/build-array-from-permutation/
+//1.method
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++){
+            nums[i]=nums[i]+(nums[nums[i]]%n)*n;
+        }
+        for(int i=0;i<nums.size();i++){
+            nums[i]=nums[i]/n;
+        }
+        return nums;
+    }
+};
+//2.method(STL use)
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            nums.push_back(nums[nums[i]]);
+        }
+        for(int i=0;i<n;i++){
+            nums.erase(nums.begin());
+        }
+        return nums;
+    }
+};
