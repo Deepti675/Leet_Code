7b7573b97caf610f43757011ab9a1ff74c21c70f
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-02-08 14:32:27 +0530

    Create add-digits.cpp

---
+++ daily_challenge/add-digits.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/add-digits/
+class Solution {
+public:
+    int addDigits(int num) {
+        int s=0;
+        while(num){
+            s+=num%10;
+            num=num/10;
+            
+            
+        }
+        if(s<10)
+        return s;
+        else
+            return addDigits(s);
+        
+    }
+};
