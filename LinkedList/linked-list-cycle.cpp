983a4d0c803d2aa2f375f97702797699f1659228
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-03-08 10:49:52 +0530

    Update linked-list-cycle.cpp

--- LinkedList/linked-list-cycle.cpp
+++ LinkedList/linked-list-cycle.cpp
@@ -22,3 +22,30 @@ public:
     return false;
     }
 };
+//fast slow pointer method
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode *h) {
+        if(h==NULL ||h->next==NULL)
+            return false;
+        ListNode* fast=h;
+        ListNode* slow=h;
+        while(fast->next!=NULL && fast->next->next!=NULL)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)
+                return true;
+        }
+        return false;
+    }
+};
