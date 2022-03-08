f5b3eca3f0ae6536fd5888a8b88246f034ca9fce
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-03-08 10:59:57 +0530

    Update linked-list-cycle.cpp

--- LinkedList/linked-list-cycle.cpp
+++ LinkedList/linked-list-cycle.cpp
@@ -49,3 +49,45 @@ public:
         return false;
     }
 };
+//complete program from iput and output
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace std;
+struct Node{
+    int data;
+    struct Node* next;
+};
+void push(struct Node** head_ref,int new_data )
+{
+        struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+        new_node->data=new_data;
+        new_node->next=(*head_ref);
+        (*head_ref)=new_node; 
+}
+bool deletLoop(struct Node* h)
+{
+    unordered_set<Node*>s;
+    while(h!=NULL)
+    {
+        if(s.find(h)!=s.end())
+        return true;
+        s.insert(h);
+        h=h->next;
+    }
+    return false;
+}
+int main()
+{
+   struct Node* head1 = NULL;
+   int n,a;
+   cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a;
+        push(&head1,a);                                                            
+    }
+    head1->next->next->next->next = head1;
+    cout<<deletLoop(head1);
+    return 0;
+}
