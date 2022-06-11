cfcab25516f807789e0426d47443b11676c979b0
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-06-12 00:36:08 +0530

    Create 3um_TwoPointer_Approch.cpp

---
+++ amazon_Array/3um_TwoPointer_Approch.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        //for assigning the value of a
+        for(int i=0;i<n-2;i++)
+        {
+            if(i==0||i>0&&nums[i]!=nums[i-1])
+            {
+                int low=i+1;
+                int high=n-1;
+                int sum=0-nums[i];
+                while(low<high)
+                {
+                    if(nums[low]+nums[high]==sum)
+                    {
+                        vector<int>v;
+                        v.push_back(nums[i]);
+                        v.push_back(nums[low]);
+                        v.push_back(nums[high]);
+                        ans.push_back(v);
+                        while(low<high&&nums[low]==nums[low+1])
+                            low++;
+                        while(low<high&&nums[high]==nums[high-1])
+                            high--;
+                        low++;
+                        high--;
+                    }
+                    else if(nums[low]+nums[high]<sum)low++;
+                        else
+                            high--;
+                }
+            }
+        }
+        return ans;
+    }
+};
