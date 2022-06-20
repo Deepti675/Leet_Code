0741a4bca99cf65b6fb93efaf88d95be265e2fa8
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-06-20 19:43:46 +0530

    Create Short Encoding of Words.cpp

---
+++ daily_challenge/Short Encoding of Words.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/short-encoding-of-words/
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>& words) {
+        ios_base:: sync_with_stdio(false);
+        cin.tie(NULL);
+        int ans=0;
+        set<string>s1(words.begin(),words.end());
+        set<string>s2(words.begin(),words.end());
+        for(auto it: s1)
+        {
+            for(int i=1;i<it.size();i++)
+                s2.erase(it.substr(i));
+        }
+        //for adding hash cahr i have to add one extra count
+        for(auto it: s2)
+        {
+            ans+=it.size()+1;
+        }
+        return ans;
+    }
+};
