61ce630fca3c11ec6b710ff792fde2ef173c74a8
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-06 11:01:45 +0530

    Create Fibonacci Number.cpp

---
+++ daily_challenge/Fibonacci Number.cpp
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/fibonacci-number/
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1)
+            return n;
+        int p1=0,p2=1,current;
+        for(int i=2;i<=n;i++)
+        {
+            current=p1+p2;
+            p1=p2;
+            p2=current;
+        }
+        return current;
+    }
+};
