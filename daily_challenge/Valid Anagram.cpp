6bdf389b13441f58807a62afda3dfa0b30aee208
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-07-28 11:13:20 +0530

    Create Valid Anagram.cpp

---
+++ daily_challenge/Valid Anagram.cpp
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/valid-anagram/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int>mpp;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            mpp[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            mpp[t[i]]--;
+        }
+      for(auto it:mpp)
+      {
+          if(it.second!=0)
+              return false;
+      }
+        return true;
+    }
+};
