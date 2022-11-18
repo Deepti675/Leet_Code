2f79c274bf2ff116e1213a686ecf754dc4765446
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2022-11-18 16:28:02 +0530

    Create Ugly Number.cpp

---
+++ daily_challenge/Ugly Number.cpp
@@ -0,0 +1,13 @@
+https://leetcode.com/problems/ugly-number/description/
+class Solution 
+{
+    public:
+    bool isUgly(int num) 
+    {
+        if (num == 0) return false;
+        for (int i : {2,3,5})
+            while (num % i == 0)
+                num /= i;
+        return num == 1;
+    }
+};
